486af5c4c85103d0c76ea2f72f21b964122af810
Kolkosan <Kolkosan@ma.ru>
2021-11-12 19:04:24 +0300

    Firstcommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello, Alex");
-        System.out.println("Hello, Alexey");
-        System.out.println("Hello, Alex");
+
+        System.out.println("Что-то новое");
     }
 }
